ea7ccba8bec6c6aae1cf71c38a72d40d66353cc0
Edsong158 <150963218+Edsong158@users.noreply.github.com>
2024-01-31 17:32:15 -0500

    finish

--- index.js
+++ index.js
@@ -36,3 +36,4 @@ async function generateLogo() {
 }
 
 generateLogo();
+
--- tests/classes.test.js
+++ tests/classes.test.js
@@ -1,22 +1,32 @@
-const { Circle, Square } = require('../lib/shapes');
+import { Circle, Square, Triangle } from '../lib/shapes';
 
-decribe('Shape Class Tests', () => {
+describe('Shape Class Tests', () => {
     it('Should render a square element tag', () => {
-        const square = new Square('xyz', 'red', 90, 75);
+        const square = new Square();
+        square.setTextColor('red');
+        square.setShapeColor('green');
+        square.setText('xyz');
 
-        square.setColor('red');
-
-        expect(square.render()).tobe('<rect width="150" height="150" fill="red" />')
+        expect(square.render()).toBe(
+            `<svg width="300" height="200">
+    <rect x="50" y="50" width="200" height="100" fill="green" stroke="red" stroke-width="2" /><text x="150" y="110" fill="red" text-anchor="middle">xyz</text></svg>`);
     });
 
     it('Should return a circle object matching specific properties', () => {
-        const circle = new Circle('xyz', 'blue', 90, 75);
+        const circle = new Circle();
+        circle.setText('xyz');
+        circle.setTextColor('blue');
+        circle.setShapeColor('red');
 
-        expect(circle).toMatchObject({
-            text: 'xyz',
-            textcolor: 'blue',
-            textX: 90,
-            textY: 75
-        });
-    })
+        expect(circle.render()).toBe(`<svg width="300" height="200"><circle cx="150" cy="100" r="80" fill="red" stroke="blue" stroke-width="2" /><text x="150" y="110" fill="blue" text-anchor="middle">xyz</text></svg>`);
+    });
+
+    it('Should return a triangle object matching specific properties', () => {
+        const triangle = new Triangle();
+        triangle.setText('xyz');
+        triangle.setTextColor('green');
+        triangle.setShapeColor('blue');
+
+        expect(triangle.render()).toBe(`<svg width="300" height="200"> <polygon points="150,10 100,190 200,190" style="fill:blue;stroke:green;stroke-width:2" /><text x="150" y="100" fill="green" text-anchor="middle">xyz</text></svg>`);
+    });
 });
